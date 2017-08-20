9c10be6c941a74a84a27c457afa5128494569c0c
malast88 <malast88@gmail.com>
2017-08-20 14:40:19 +0200

    Added Numeral class

---
+++ algos/classes.Tests/NumeralTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+using classes.Digits;
+using System.Collections.Generic;
+
+namespace classes.Tests
+{
+    [TestClass]
+    public class NumeralTest
+    {
+        [TestMethod]
+        public void NumeralShouldGetDigits()
+        {
+            #region Arrange
+            var ulongNum = 29382440;
+            var bigIntegerNum = new BigInteger(43653452598);
+            #endregion
+            #region Act
+            var ulongDigits = Numeral.GetDigits(ulongNum);
+            var bigIntegerDigits = Numeral.GetDigits(bigIntegerNum);
+            #endregion
+            #region Assert
+            Assert.AreEqual(8, ulongDigits.Count);
+            Assert.AreEqual(0, ulongDigits[0]);
+            Assert.AreEqual(4, ulongDigits[1]);
+            Assert.AreEqual(4, ulongDigits[2]);
+            Assert.AreEqual(2, ulongDigits[3]);
+            Assert.AreEqual(2, ulongDigits[7]);
+
+            Assert.AreEqual(11, bigIntegerDigits.Count);
+            Assert.AreEqual(8, bigIntegerDigits[0]);
+            Assert.AreEqual(9, bigIntegerDigits[1]);
+            Assert.AreEqual(6, bigIntegerDigits[8]);
+            Assert.AreEqual(3, bigIntegerDigits[9]);
+            Assert.AreEqual(4, bigIntegerDigits[10]);
+            #endregion
+        }
+
+        [TestMethod]
+        public void NumeralShouldSumDigits()
+        {
+            #region Arrange
+            var digits1 = new List<List<byte>>();
+            var digits2 = new List<List<byte>>();
+            var sums = new List<List<byte>>();
+            var results = new List<List<byte>>();
+
+            digits1.Add(new List<byte>() { 0, 0, 0, 1 });
+            digits2.Add(new List<byte>() { 2, 4, 1 });
+            sums.Add(new List<byte>());
+            results.Add(new List<byte>() { 2, 4, 1, 1 });
+
+            digits1.Add(new List<byte>() { 8, 7, 5, 6 });
+            digits2.Add(new List<byte>() { 7, 9 });
+            sums.Add(new List<byte>());
+            results.Add(new List<byte>() { 5, 7, 6, 6 });
+
+            digits1.Add(new List<byte>() { 9, 9, 9 });
+            digits2.Add(new List<byte>() { 9, 9, 9 });
+            sums.Add(new List<byte>());
+            results.Add(new List<byte>() { 8, 9, 9, 1 });
+
+            digits1.Add(new List<byte>() { 9, 9, 9 });
+            digits2.Add(new List<byte>() { 9, 9 });
+            sums.Add(new List<byte>());
+            results.Add(new List<byte>() { 8, 9, 0, 1 });
+            #endregion
+            #region Act
+            for (int i=0;i<digits1.Count;i++)
+            {
+                Numeral.SumDigits(digits1[i], digits2[i], sums[i]);
+            }
+            #endregion
+            #region Assert
+            for (int i = 0; i < digits1.Count; i++)
+            {
+                Assert.AreEqual(string.Join(":", results[i]), string.Join(":", sums[i]));
+            }
+            #endregion
+        }
+
+        [TestMethod]
+        public void NumeralShouldRetainValue()
+        {
+            #region Arrange
+            var n1 = new BigInteger(234235);
+            var n2 = 478365;
+            #endregion
+            #region Act
+            var num1 = new Numeral(n1);
+            var num2 = new Numeral(n2);
+            var nn1 = num1.GetBigInteger();
+            var nn2 = num2.GetBigInteger();
+            #endregion
+            #region Assert
+            Assert.AreEqual(n1, nn1);
+            Assert.AreEqual(n2, nn2);
+            #endregion
+        }
+
+        [TestMethod]
+        public void NumeralShouldCheckIfPalyndrome()
+        {
+            #region Arrange
+            #endregion
+            #region Act
+            #endregion
+            #region Assert
+            Assert.AreEqual(true, new Numeral(4578754).IsPalyndrome());
+            Assert.AreEqual(true, new Numeral(44766744).IsPalyndrome());
+            Assert.AreEqual(false, new Numeral(3940573458).IsPalyndrome());
+            #endregion
+        }
+    }
+}
---
+++ algos/classes/Digits/Numeral.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace classes.Digits
+{
+    /// <summary>
+    /// Class wich represents decimal positive integer internally as List of digits.
+    /// Used in scenarios with high usage of number's digits structure
+    /// (for example, polyndrome checking, reverse of digits etc).
+    /// </summary>
+    public class Numeral
+    {
+        #region Constructors
+
+        public Numeral(ulong n)
+        {
+            Digits = GetDigits(n);
+        }
+
+        public Numeral(BigInteger n)
+        {
+            Digits = GetDigits(n);
+        }
+
+        #endregion
+
+        #region Public
+
+        public List<byte> Digits { get; set; }
+
+        public BigInteger GetBigInteger()
+        {
+            BigInteger result = new BigInteger(0);
+            BigInteger multiplier = new BigInteger(1);
+            for (int i = 0; i < Digits.Count; i++)
+            {
+                result += Digits[i] * multiplier;
+                multiplier *= 10;
+            }
+            return result;
+        }
+
+        public ulong GetULong()
+        {
+            ulong result = 0;
+            ulong multiplier = 1;
+            checked
+            {
+                for (int i = 0; i < Digits.Count; i++)
+                {
+                    result += Digits[i] * multiplier;
+                    multiplier *= 10;
+                }
+            }
+            return result;
+        }
+
+        public bool IsPalyndrome()
+        {
+            for (int i = 0; i < Digits.Count / 2; i++)
+            {
+                if (Digits[i] != Digits[Digits.Count - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region Static utils
+
+        public static List<byte> GetDigits(ulong n)
+        {
+            var result = new List<byte>();
+            while (n > 0)
+            {
+                result.Add((byte)(n % 10));
+                n /= 10;
+            }
+            return result;
+        }
+
+        public static List<byte> GetDigits(BigInteger n)
+        {
+            var result = new List<byte>();
+            while (n > 0)
+            {
+                result.Add((byte)(n % 10));
+                n /= 10;
+            }
+            return result;
+        }
+
+        public static void Sum(Numeral n1, Numeral n2, Numeral result)
+        {
+            SumDigits(n1.Digits, n2.Digits, result.Digits);
+        }
+
+        public static void SumDigits(List<byte> digits1, List<byte> digits2, List<byte> result)
+        {
+            for (int i = 0; i < result.Count; i++)
+            {
+                result[i] = 0;
+            }
+
+            int currIndex = 0;
+            var maxIndex = Math.Max(digits1.Count, digits2.Count);
+            while (currIndex < maxIndex)
+            {
+                if (result.Count == currIndex)
+                {
+                    result.Add(0);
+                }
+
+                int sum = 0;
+                if (currIndex < digits1.Count)
+                {
+                    sum += digits1[currIndex];
+                }
+                if (currIndex < digits2.Count)
+                {
+                    sum += digits2[currIndex];
+                }
+                if (sum < 10)
+                {
+                    result[currIndex] += (byte)sum;
+                    if (result[currIndex] >= 10)
+                    {
+                        result[currIndex] = (byte)(result[currIndex] % 10);
+                        if (result.Count == currIndex + 1)
+                        {
+                            result.Add(0);
+                        }
+                        result[currIndex + 1] = 1;
+                    }
+                }
+                else
+                {
+                    result[currIndex] += (byte)(sum % 10);
+                    if (result.Count == currIndex + 1)
+                    {
+                        result.Add(0);
+                    }
+                    result[currIndex + 1] = 1;
+                }
+
+                currIndex++;
+            }
+        }
+
+        #endregion
+    }
+}
